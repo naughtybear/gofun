794c89742796d79ba8dba1f1f0f9924cc2ad7ff5
EricHan <erichan_bj@outlook.com>
2017-04-08 13:00:38 +0800

    binary search

---
+++ algo/binary_search.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 22, 32}
+	fmt.Println(binarySearch(arr, 7))
+	fmt.Println(binarySearch(arr, 8))
+	fmt.Println(binarySearch(arr, 4))
+	fmt.Println(binarySearch(arr, 22))
+}
+
+func binarySearch(arr []int, x int) int {
+	i, j := 0, len(arr)
+
+	for i < j {
+		k := (i + j) / 2
+		if arr[k] == x {
+			return k
+		} else if arr[k] < x {
+			i = k + 1
+		} else {
+			j = k - 1
+		}
+	}
+
+	return -1
+}
